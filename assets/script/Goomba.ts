166e7b717324b2ed97ff639179845b67ff205e3a
WeakGT <jasonhuang921003@gmail.com>
2024-05-22 19:44:33 +0800

    add Goomba

---
+++ assets/script/Goomba.ts
@@ -0,0 +1,43 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export class Goomba extends cc.Component {
+
+    @property
+    moveSpeed: number = 100;
+
+    @property(cc.SpriteFrame)
+    diedGoombaSprite: cc.SpriteFrame = null;
+
+    private body: cc.RigidBody = null;
+    private moveDirection: number = 1;
+
+    onLoad() {
+        this.body = this.getComponent(cc.RigidBody);
+        this.body.linearVelocity = cc.v2(this.moveSpeed, 0);
+    }
+
+    onBeginContact(contact, selfCollider, otherCollider) {
+        // console.log(otherCollider.node.group);
+        if (otherCollider.node.name == "Boundary") {
+            this.moveDirection *= -1;
+            this.body.linearVelocity = cc.v2(this.moveDirection * this.moveSpeed, 0);
+        }
+        else if (otherCollider.node.name == "Mario") {
+            if (contact.getWorldManifold().normal.y > 0.8) {
+                this.node.runAction(cc.callFunc(this.changeSprite, this));
+            }
+            else { // Mario die
+
+            }
+        }
+    }
+
+    changeSprite() {
+        this.body.linearVelocity = cc.v2(0, 0);
+        this.getComponent(cc.Sprite).spriteFrame = this.diedGoombaSprite;
+        this.scheduleOnce(() => {
+            this.node.destroy();
+        }, 0.3);
+    }
+}
