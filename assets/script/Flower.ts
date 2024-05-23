7a204d724682f496912025e50e9c2181e55ba4c5
WeakGT <jasonhuang921003@gmail.com>
2024-05-23 18:05:32 +0800

    add Flower

---
+++ assets/script/Flower.ts
@@ -0,0 +1,38 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export class Flower extends cc.Component {
+
+    // @property(cc.Integer)
+    // scoreValue: number = 100;
+
+    @property(cc.Vec2)
+    moveRange: cc.Vec2 = cc.v2(0, 100);
+
+    @property(cc.Float)
+    moveDuration: number = 2.0;
+
+    private initialPosition: cc.Vec2 = null;
+
+    onLoad() {
+        this.getComponent(cc.Animation).play("Flower");
+        this.startMoving();
+    }
+
+    startMoving() {
+        const action = cc.repeatForever(
+            cc.sequence(
+                cc.moveBy(1, 0, -34),
+                cc.delayTime(1),
+                cc.moveBy(1, 0, 34),
+                cc.delayTime(1))
+        );
+        this.node.runAction(action);
+    }
+    
+    onBeginContact(contact, selfCollider, otherCollider) {
+        if (otherCollider.node.name === "Mario") { // Mario die
+
+        }
+    }
+}
